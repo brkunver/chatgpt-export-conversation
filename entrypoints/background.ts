313281176bda487579851a9290e84798221718db
brkunver <burakhanunver@gmail.com>
2025-04-20 14:20:20 +0300

    init background

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,12 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+  browser.runtime.onMessage.addListener((req, sender, response) => {
+    if (req.action == "logContent") {
+      browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+        const tabId = tabs[0]?.id
+        if (tabId) {
+          browser.tabs.sendMessage(tabId, { action: "log", includeUser: req.includeUser })
+        }
+      })
+    }
+  })
+})
