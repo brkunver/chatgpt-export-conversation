6e540142de86b365a9bf82f0f90efbf7312d7a56
brkunver <burakhanunver@gmail.com>
2025-04-20 14:20:38 +0300

    init popup

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,9 +1,28 @@
 <script lang="ts" setup>
-import "~/assets/tailwind.css"
+import { ref } from "vue"
+import Toggle from "@/components/toggle.vue"
+
+const checked = ref(false)
+const userPromptsId = "user-prompts"
+
+function logChatHandler() {
+  browser.runtime.sendMessage({ action: "logContent", includeUser: checked.value })
+}
+
+function toggleHandler(check: boolean) {
+  checked.value = check
+}
 </script>
 
 <template>
-  <main class="min-w-[300px] p-4">
+  <main class="flex min-w-[300px] flex-col justify-center gap-4 p-4 text-center">
     <h1 class="text-2xl font-bold">ChatGPT Export Conversation</h1>
+    <div class="flex items-center gap-2">
+      <Toggle :id="userPromptsId" :set-checked="toggleHandler" />
+      <label :for="userPromptsId" class="text-lg text-blue-900 select-none">Include User Prompts</label>
+    </div>
+    <button class="cursor-pointer rounded bg-black px-2 py-2 text-center text-lg text-white" @click="logChatHandler">
+      Download As TXT
+    </button>
   </main>
 </template>
