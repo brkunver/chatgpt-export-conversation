fede2b915b54fd67cb27312b18ace390b8b871f2
brkunver <burakhanunver@gmail.com>
2025-04-25 13:20:35 +0300

    add permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite"
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-vue"],
+  manifest: {
+    permissions: ["activeTab", "tabs"],
+  },
   webExt: {
     disabled: true,
   },
