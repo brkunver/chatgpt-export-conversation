38ed2367944176f36ddd3354084cd97b286789e7
brkunver <burakhanunver@gmail.com>
2025-04-25 13:21:20 +0300

    fix tabs messaging

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,14 +1,35 @@
+import devlog from "@/utils/dev-log"
+
 export default defineBackground(() => {
   browser.runtime.onMessage.addListener((req, sender, response) => {
     if (req.action == "logContent") {
-      browser.tabs.query({ active: true, currentWindow: true }, tabs => {
-        const tabId = tabs[0]?.id
+      devlog("background received logContent request")
+
+      devlog("background => req", req)
+      browser.tabs.query({}, tabs => {
+        const targetTab = tabs.find(tab => tab.url?.startsWith("https://chatgpt.com/c/"))
+        if (!targetTab) {
+          devlog("No matching tab found")
+          return
+        }
+        const tabId = targetTab.id
+        devlog("Matching tab ID: ", tabId)
         if (tabId) {
-          browser.tabs.sendMessage(tabId, {
-            action: "log",
-            includeUser: req.includeUser,
-            includeRoleNames: req.includeRoleNames,
-          })
+          browser.tabs
+            .sendMessage(tabId, {
+              action: "log",
+              includeUser: req.includeUser,
+              includeRoleNames: req.includeRoleNames,
+            })
+            .then(() => {
+              devlog("background => logContent done")
+            })
+            .catch(() => {
+              devlog("background => logContent error")
+            })
+            .finally(() => {
+              devlog("background => logContent finally")
+            })
         }
       })
     }
