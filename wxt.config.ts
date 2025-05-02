fd4840ff8a3094e2e74366f2aa4078bc29c00b88
brkunver <burakhanunver@gmail.com>
2025-05-02 17:16:10 +0300

    add i18n module

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/i18n/module"],
   manifest: {
+    default_locale: "en",
     permissions: ["tabs"],
+    name: "__MSG_extensionName__",
+    description: "__MSG_extensionDescription__",
   },
   webExt: {
     disabled: true,
