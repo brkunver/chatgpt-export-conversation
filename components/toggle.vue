98d068ecee898946354a76276003b47e329e104b
brkunver <burakhanunver@gmail.com>
2026-05-17 14:35:27 +0300

    apply warning fixes

--- components/toggle.vue
+++ components/toggle.vue
@@ -17,11 +17,11 @@ function onChange(e: Event) {
 <template>
   <label
     :for="props.id"
-    class="relative block h-8 w-14 cursor-pointer rounded-full border border-white/10 bg-slate-700/90 shadow-inner transition-colors duration-200 [-webkit-tap-highlight-color:_transparent] has-checked:bg-emerald-500"
+    class="relative block h-8 w-14 cursor-pointer rounded-full border border-white/10 bg-slate-700/90 shadow-inner transition-colors duration-200 [-webkit-tap-highlight-color:transparent] has-checked:bg-emerald-500"
   >
     <input type="checkbox" :id="props.id" class="peer sr-only" :checked="props.checked" @change="onChange" />
     <span
-      class="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-white shadow-[0_6px_16px_rgba(15,23,42,0.35)] ring-1 ring-black/5 transition-[inset-inline-start,transform] duration-200 peer-checked:start-6"
+      class="absolute inset-y-0 inset-s-0 m-1 size-6 rounded-full bg-white shadow-[0_6px_16px_rgba(15,23,42,0.35)] ring-1 ring-black/5 transition-[inset-inline-start,transform] duration-200 peer-checked:inset-s-6"
     ></span>
   </label>
 </template>
